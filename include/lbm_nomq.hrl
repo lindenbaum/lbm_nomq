fdd867fa1e2a4de0e7bd3e8f9be455f4c49a59d7
Tobias Schlager <tobias.schlager@lindenbaum.eu>
2014-10-13 09:48:23 +0200

    Move backend definition into lbm_nomq_dist

--- include/lbm_nomq.hrl
+++ include/lbm_nomq.hrl
@@ -13,9 +13,6 @@
 -ifndef(lbm_nomq_hrl_).
 -define(lbm_nomq_hrl_, 1).
 
--define(BACKEND, lbm_nomq_ets).
-%%-define(BACKEND, lbm_nomq_kv).
-
 -record(lbm_nomq_subscr, {m :: module(), f :: atom(), as :: [term()]}).
 
 -define(UPDATE_MSG(Ref, Topic, Ss), {lbm_nomq, Ref, Topic, Ss}).
